ef010faeecf213cc3d1d15d6c0d2007b7511fc3a
AndrewDyomin <dyomin.andrew1@gmail.com>
2025-07-10 14:35:33 +0300

    Update InventoryCheckDetails.js

--- src/components/InventoryCheckDetails/InventoryCheckDetails.js
+++ src/components/InventoryCheckDetails/InventoryCheckDetails.js
@@ -118,7 +118,7 @@ export const InventoryCheckDetails = ({ id }) => {
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', 'inventory.xlsx');
+    link.setAttribute('download');
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
