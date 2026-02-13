14e9b934904e9d1fed07c6c8a4a3b490b3fbb5a0
AndrewDyomin <dyomin.andrew1@gmail.com>
2026-02-13 20:03:42 +0200

    Update PuigApi.js

--- src/components/PuigApi/PuigApi.js
+++ src/components/PuigApi/PuigApi.js
@@ -1,9 +1,32 @@
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export const PuigApiHome = () => {
   const { t } = useTranslation();
+  const [categ, setCateg] = useState([]);
+
+  useEffect(() => {
+    async function getCateg() {
+      const categArray = await axios.get('/puig-api/categories');
+      setCateg(categArray.data);
+    }
+
+    if (categ.length === 0) {
+      getCateg();
+    }
+
+    console.log(categ);
+  }, [categ]);
 
   return (
-    <>helo Puig</>
+    <>
+      helo Puig
+      <ul>
+        {categ.map(i => (
+          <li key={i.id}>{i.title}</li>
+        ))}
+      </ul>
+    </>
   );
 };
